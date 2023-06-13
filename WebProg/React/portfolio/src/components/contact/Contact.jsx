d1deadef8344b569378c716e6bf99e134ff8ee7e
Anish <anishrachcha@gmail.com>
2023-06-13 23:19:50 +0530

    Portfolio

---
+++ WebProg/React/portfolio/src/components/contact/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Contact.scss"
+//rfc==>react functional component
+
+export default function Contact() {
+  return (
+    <div className='contact' id='contact'>
+      
+    </div>
+  )
+}
---
+++ WebProg/React/portfolio/src/components/topbar/Topbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Topbar.scss'
+
+export default function Topbar() {
+  return (
+    <div className='topbar'>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#"></a>
+        </div>
+        <div className="right"></div>
+      </div>
+      
+    </div>
+  )
+}
